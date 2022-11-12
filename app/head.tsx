8ba30cff4bea360f71519940b9790d2e550dac41
GregShen <gregshen0925@gmail.com>
2022-11-12 10:14:33 +0800

    setting up next13

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Aptos Messenger</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const HomePage = (props: Props) => {
+    return (
+        <div>
+            <h1>This is an app for showing Aptos Token Design </h1>
+        </div>
+    )
+}
+
+export default HomePage
