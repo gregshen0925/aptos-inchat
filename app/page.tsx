1793c2e3aa58f1822a5618312c2a93ec9445df66
GregShen <gregshen0925@gmail.com>
2022-11-13 23:41:22 +0800

    create context

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,21 @@
-"use client"
-import React from 'react'
-import ChatInput from './ChatInput'
-import MessageList from './MessageList'
-import '../styles/styles.css'
-import {
-    WalletAdapterNetwork,
-    MartianWalletAdapter,
-    AptosWalletAdapter,
-    RiseWalletAdapter,
-    FewchaWalletAdapter,
-    WalletProvider,
-    PontemWalletAdapter,
-    SpikaWalletAdapter,
-    BitkeepWalletAdapter,
-    BloctoWalletAdapter
-  } from '@manahippo/aptos-wallet-adapter';
-  import { useMemo } from 'react';
+"use client";
+import React from "react";
+import ChatInput from "./ChatInput";
+import MessageList from "./MessageList";
+import "../styles/styles.css";
+import { AptosWalletProvider } from "../context/AptosWalletProvider";
 
-
-type Props = {}
+type Props = {};
 
 const HomePage = (props: Props) => {
-    const wallets = useMemo(
-        () => [
-        //   new BloctoWalletAdapter({ network: WalletAdapterNetwork.Mainnet }),
-          new MartianWalletAdapter(),
-          new AptosWalletAdapter(),
-          new RiseWalletAdapter(),
-          new FewchaWalletAdapter(),
-          new PontemWalletAdapter(),
-          new SpikaWalletAdapter(),
-          new BitkeepWalletAdapter(),
-        ],
-        []
-      );
-    return (
-        <WalletProvider
-        wallets={wallets}
-        onError={(error: Error) => {
-          console.log('wallet errors: ', error);
-        }}>
-        <main>
-            <MessageList />
-            <ChatInput />
-        </main>
-        </WalletProvider>
+  return (
+    <main>
+      <AptosWalletProvider>
+        <MessageList />
+        <ChatInput />
+      </AptosWalletProvider>
+    </main>
+  );
+};
 
-        
-
-    )
-}
-
-export default HomePage
+export default HomePage;
--- context/AptosWalletProvider.tsx
+++ context/AptosWalletProvider.tsx
@@ -1,18 +1,46 @@
-"use client"
+"use client";
+import React, { createContext, FC } from "react";
+import {
+  WalletAdapterNetwork,
+  MartianWalletAdapter,
+  AptosWalletAdapter,
+  RiseWalletAdapter,
+  FewchaWalletAdapter,
+  WalletProvider,
+  PontemWalletAdapter,
+  SpikaWalletAdapter,
+  BitkeepWalletAdapter,
+  BloctoWalletAdapter,
+  WalletProviderProps,
+} from "@manahippo/aptos-wallet-adapter";
+import { useMemo } from "react";
 
+type WalletProvider = {
+  children: React.ReactNode;
+};
 
-import React from 'react'
-
-interface Props {
-    
-}
-
-const AptosWalletProvider = (props: Props) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default AptosWalletProvider
+export const AptosWalletProvider: FC<WalletProvider> = ({ children }) => {
+  const wallets = useMemo(
+    () => [
+      //   new BloctoWalletAdapter({ network: WalletAdapterNetwork.Mainnet }),
+      new MartianWalletAdapter(),
+      new AptosWalletAdapter(),
+      new RiseWalletAdapter(),
+      new FewchaWalletAdapter(),
+      new PontemWalletAdapter(),
+      new SpikaWalletAdapter(),
+      new BitkeepWalletAdapter(),
+    ],
+    []
+  );
+  return (
+    <WalletProvider
+      wallets={wallets}
+      onError={(error: Error) => {
+        console.log("wallet errors: ", error);
+      }}
+    >
+      {children}
+    </WalletProvider>
+  );
+};
