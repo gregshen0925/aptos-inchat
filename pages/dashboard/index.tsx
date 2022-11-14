d5bd96afe56ac937ae20c30fe369ad0645fd4d7b
GregShen <gregshen0925@gmail.com>
2022-11-14 21:46:03 +0800

    fix bug

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -61,6 +61,9 @@ const Home = ({ messages }: Props) => {
 
   return (
     <div className="">
+      <title>InJoin Dashboard</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="https://i.imgur.com/SjaOCZH.png" />
       <Header
         setConnectModalOn={setConnectModalOn}
         setInviteModalOn={setInviteModalOn}
